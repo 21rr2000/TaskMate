713fa21237179a30949ca3ca81bfb39ef56d0158
21rr2000 <rr21cskiit@gmail.com>
2024-08-29 21:16:55 +0530

    http servers

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express=require("express");
+const { createTodo, updateTodo } = require("./types");
 const app=express();
 
 app.use(express.json());
@@ -6,13 +7,27 @@ app.use(express.json());
 //      description:string
 //  }
 app.post("/todo",function (req,res){
-
+    const createPayload=req.body;
+    const parsePayload=createTodo.safeParse(createPayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg:"You sent the wrong input"
+        })
+        return;
+    }
 })
 
 app.get("/todos",function(req,res){
-
+    
 })
 
 app.put("/completed",function(req,res){
-
+    const updatePayload=req.body;
+    const parsePayload=updateTodo.safeParse(updatePayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg:"You sent the wrong inputs"
+        })
+        return;
+    }
 })
